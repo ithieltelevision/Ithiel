8897cd6dcaa6aa9af6e6cfbd2120dbaa050855e9
ithieltelevision <ithieltelevision@gmail.com>
2026-04-23 01:52:51 -0500

    Sincronización manual 23-04-2026 01:52:50

--- subir.sh
+++ subir.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd /sdcard/Ithiel\ Televisión
 git add .
+git commit -m "Sincronización manual $(date +'%d-%m-%Y %H:%M:%S')"
+git push -f origin master
+#!/bin/bash
+cd /sdcard/Ithiel\ Televisión
+git add .
 git commit -m "Actualización automática $(date +'%d-%m-%Y %H:%M:%S')"
 git push origin master
 
